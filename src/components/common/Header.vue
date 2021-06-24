404f423afc8bc507cdc77538bd4abc349d5d530f
dxw <1158514898@qq.com>
2021-06-24 09:05:35 +0800

    修改配置文件

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -5,7 +5,7 @@
             <i v-if="!collapse" class="el-icon-s-fold"></i>
             <i v-else class="el-icon-s-unfold"></i>
         </div>
-        <div class="logo">启明WMS</div>
+        <div class="logo">WMS</div>
         <div class="header-right">
             <div class="header-user-con">
                 <!-- 全屏显示 -->
